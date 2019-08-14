b575e9948bafcbbda3abc71fc214e70347a2b6b2
Belikov Vyacheslav <humanunidentifed@gmail.com>
2019-08-14 09:52:52 +0300

    Доработано получение последней директории.
    Упрощена сортировка
    Убран лишний slice для директорий

--- main.go
+++ main.go
@@ -3,45 +3,33 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"sort"
 	"time"
 )
 
-// Для сортировки директорий по дате\времени создания
-type byCreateDate []os.FileInfo
-
-func (s byCreateDate) Len() int {
-	return len(s)
-}
-func (s byCreateDate) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s byCreateDate) Less(i, j int) bool {
-	diff := s[i].ModTime().Sub(s[j].ModTime())
-	return diff < 0
-}
-
 // Получение названия последней директории по указанному пути
-func getLastDir(path string) (lastDir string) {
+func getLastDirName(path string) (lastDir string) {
 	allFiles, _ := ioutil.ReadDir(path)
-	sort.Sort(byCreateDate(allFiles))
-	dirs := make([]string, 0)
+
+	sort.SliceStable(allFiles, func(i, j int) bool {
+		diff := allFiles[i].ModTime().Sub(allFiles[j].ModTime())
+		return diff > 0
+	})
 
 	for _, f := range allFiles {
 		if f.IsDir() {
-			dirs = append(dirs, f.Name())
+			return f.Name()
 		}
 	}
 
-	return dirs[len(dirs)-1]
+	return ""
 }
 
 func watchDir(path string) {
 	fmt.Println("Start watch folder ", path)
 	watcherInterval := time.Second * 5
 	for true {
-		lastDir := getLastDir(path)
+		lastDir := getLastDirName(path)
 		if lastDir != "" {
 			fmt.Println("lastDir=", lastDir)
 		}
